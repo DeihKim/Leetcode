dcbfd0134a0f07f8c4b3c5eb4671d9a520a2ca0c
Cing Sian Deih Kim <deihkim442@gmail.com>
2025-06-10 13:51:53 +0630

    Time: 35 ms (68.35%) | Memory: 14 MB (57.91%) - LeetSync

---
+++ 1231-replace-elements-with-greatest-element-on-right-side/replace-elements-with-greatest-element-on-right-side.py
@@ -0,0 +1,12 @@
+class Solution(object):
+    def replaceElements(self, arr):
+        """
+        :type arr: List[int]
+        :rtype: List[int]
+        """
+        maxRight = -1
+        for i in range (len(arr) - 1, -1, -1):
+            current = arr[i]
+            arr[i] = maxRight
+            maxRight = max(maxRight, current)
+        return arr
