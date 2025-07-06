1eee6c8d675c35af4ffbe43a06b7f8033147b774
Cing Sian Deih Kim <deihkim442@gmail.com>
2025-07-06 20:03:00 +0630

    2886-FAULTY-KEYBOARD Stats: Time: 16 ms (10.74%), Space: 12.6 MB (0%) - LeetHub

--- 2886-faulty-keyboard/2886-faulty-keyboard.py
+++ 2886-faulty-keyboard/2886-faulty-keyboard.py
@@ -6,8 +6,8 @@ class Solution(object):
         """
         res = ""
         for i in range(len(s)):
-            if s[i] != 'i':
-                res += s[i]
-            else:
+            if s[i] == 'i':
                 res = res[::-1]
+            else:
+                res += s[i]
         return res
