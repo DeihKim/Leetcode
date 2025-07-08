9948621eef441596e3505deaba3e0b19c0b1e2c4
Cing Sian Deih Kim <deihkim442@gmail.com>
2025-07-08 19:29:17 +0630

    2476-CHECK-DISTANCES-BETWEEN-SAME-LETTERS Stats: Time: 0 ms (100%), Space: 12.5 MB (16.98%) - LeetHub

--- 2476-check-distances-between-same-letters/2476-check-distances-between-same-letters.py
+++ 2476-check-distances-between-same-letters/2476-check-distances-between-same-letters.py
@@ -7,8 +7,13 @@ class Solution(object):
         """
         char = set(s)
         for c in char:
-            index = s.index(c)
-            dis = distance[ord(c) - 97] #unicode of a is 97
+            index = s.index(c) # return the index where c first occurs
+            
+            # return the distance of the letter c in the distance array
+            dis = distance[ord(c) - 97] # unicode of a is 97
+
+            # check if the the index might be out of range
+            # check if the second occurance of c is in the correct place
             if index + dis + 1 > len(s) - 1 or s[index + dis + 1] != c:
                 return False
         return True
