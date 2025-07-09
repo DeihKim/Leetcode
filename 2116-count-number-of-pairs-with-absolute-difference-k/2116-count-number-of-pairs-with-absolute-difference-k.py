f3b61ecc149cd134eb0726e448d5f48484dcee9a
Cing Sian Deih Kim <deihkim442@gmail.com>
2025-07-09 11:55:54 +0630

    2116-COUNT-NUMBER-OF-PAIRS-WITH-ABSOLUTE-DIFFERENCE-K Stats: Time: 7 ms (79.13%), Space: 12.5 MB (57.99%) - LeetHub

--- 2116-count-number-of-pairs-with-absolute-difference-k/2116-count-number-of-pairs-with-absolute-difference-k.py
+++ 2116-count-number-of-pairs-with-absolute-difference-k/2116-count-number-of-pairs-with-absolute-difference-k.py
@@ -5,6 +5,19 @@ class Solution(object):
         :type k: int
         :rtype: int
         """
+
+        seen = defaultdict(int)
+        count = 0
+        for num in nums:
+            # this adds all valid previous matches in one go
+            # e.g: num = 2, k = 1
+            # so num - k = 2 - 1 = 1 and num + k = 2 + 1 = 3
+            count += seen[num - k] + seen[num + k]
+            seen[num] += 1
+        return count
+
+        '''
+        Original
         n = len(nums)
         count = 0
         for i in range(n):
@@ -12,3 +25,4 @@ class Solution(object):
                 if abs(nums[j] - nums[i]) == k:
                     count += 1
         return count
+        '''
