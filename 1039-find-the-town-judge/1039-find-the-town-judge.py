ceaf71e38e974ff94f7792dc848c115015837f62
Cing Sian Deih Kim <deihkim442@gmail.com>
2025-07-10 18:19:56 +0630

    1039-FIND-THE-TOWN-JUDGE Stats: Time: 27 ms (28.34%), Space: 15.9 MB (93.76%) - LeetHub

--- 1039-find-the-town-judge/1039-find-the-town-judge.py
+++ 1039-find-the-town-judge/1039-find-the-town-judge.py
@@ -5,12 +5,15 @@ class Solution(object):
         :type trust: List[List[int]]
         :rtype: int
         """
-        pTj = [0] * (n + 1)
+        trusts = [0] * (n + 1)
+
         for i in range(len(trust)):
-            pTj[trust[i][0]] -= 1
-            pTj[trust[i][1]] += 1
-        for i in range(1, len(pTj)):
-            trustNum = pTj[i]
+            trusts[trust[i][0]] -= 1
+            trusts[trust[i][1]] += 1
+
+        for i in range(1, len(trusts)):
+            trustNum = trusts[i]
             if trustNum == n - 1:
                 return i
+
         return -1
