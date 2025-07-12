668c24b4f12b6d3873886f73daf9e079f1434c4f
Cing Sian Deih Kim <deihkim442@gmail.com>
2025-07-12 11:39:43 +0630

    0920-UNCOMMON-WORDS-FROM-TWO-SENTENCES Stats: Time: 0 ms (100%), Space: 12.2 MB (99.46%) - LeetHub

--- 0920-uncommon-words-from-two-sentences/0920-uncommon-words-from-two-sentences.py
+++ 0920-uncommon-words-from-two-sentences/0920-uncommon-words-from-two-sentences.py
@@ -1,22 +1,33 @@
 class Solution(object):
     from collections import Counter
-
+    '''
     def uniqueWord(self, str1, str2):
         unique = []
         for word in str1:
             if str1[word] == 1 and word not in str2:
                 unique.append(word)
         return unique
-
+    '''
     def uncommonFromSentences(self, s1, s2):
         """
         :type s1: str
         :type s2: str
         :rtype: List[str]
         """
+        '''
         s1 = Counter(s1.split())
         s2 = Counter(s2.split())
         uncommonWords = []
         uncommonWords += self.uniqueWord(s1, s2)
         uncommonWords += self.uniqueWord(s2, s1)
         return uncommonWords
+
+        '''
+
+        str = Counter(s1.split() + s2.split())
+        uniqueWords = []
+        for word in str:
+            if str[word] == 1:
+                uniqueWords.append(word)
+        return uniqueWords
+        
