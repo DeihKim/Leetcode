00a08e0cd35894b44b1930f49226559143db5aac
Cing Sian Deih Kim <deihkim442@gmail.com>
2025-07-13 19:32:54 +0630

    0350-INTERSECTION-OF-TWO-ARRAYS-II Stats: Time: 4 ms (47.32%), Space: 12.5 MB (87.07%) - LeetHub

--- 0350-intersection-of-two-arrays-ii/0350-intersection-of-two-arrays-ii.py
+++ 0350-intersection-of-two-arrays-ii/0350-intersection-of-two-arrays-ii.py
@@ -8,8 +8,17 @@ class Solution(object):
         :rtype: List[int]
         """
         nums1 = Counter(nums1)
+        res = []
+        for num in nums2:
+            if nums1[num] > 0:
+                res.append(num)
+                nums1[num] -= 1
+        return res
+        '''
+        nums1 = Counter(nums1)
         nums2 = Counter(nums2)
         res = []
         for num, fq in nums1.items():
             res.append([num] * min(fq, nums2[num]))
         return sum(res, [])
+        '''
