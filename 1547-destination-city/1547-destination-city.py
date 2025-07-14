54a17555df0a310e2f9ba79ebb7e4dbd5415a08e
Cing Sian Deih Kim <deihkim442@gmail.com>
2025-07-14 17:26:45 +0630

    1547-DESTINATION-CITY Stats: Time: 0 ms (100%), Space: 12.6 MB (20.21%) - LeetHub

--- 1547-destination-city/1547-destination-city.py
+++ 1547-destination-city/1547-destination-city.py
@@ -5,9 +5,18 @@ class Solution(object):
         :rtype: str
         """
         start = set()
+        for city1, city2 in paths:
+            start.add(city1)
+        for city1, city2 in paths:
+            if city2 not in start:
+                return city2
+        '''
+        Origianl
+        start = set()
         for path in paths:
             start.add(path[0])
         for path in paths:
             if path[1] not in start:
                 return path[1]
+        '''
 
