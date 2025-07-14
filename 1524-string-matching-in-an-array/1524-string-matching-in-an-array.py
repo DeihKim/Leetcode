245c68c73d0b348077d85a0adc696b21956f379e
Cing Sian Deih Kim <deihkim442@gmail.com>
2025-07-14 17:42:35 +0630

    1524-STRING-MATCHING-IN-AN-ARRAY Stats: Time: 3 ms (80.53%), Space: 12.4 MB (81.68%) - LeetHub

--- 1524-string-matching-in-an-array/1524-string-matching-in-an-array.py
+++ 1524-string-matching-in-an-array/1524-string-matching-in-an-array.py
@@ -4,6 +4,15 @@ class Solution(object):
         :type words: List[str]
         :rtype: List[str]
         """
+        res = []
+        for i in range(len(words)):
+            for j in range(len(words)):
+                if i != j and words[i] in words[j]:
+                    res.append(words[i])
+                    break
+        return res
+        '''
+        Original
         n = len(words)
         res = set()
         for i, word in enumerate(words):
@@ -11,3 +20,4 @@ class Solution(object):
                 if i != j and word in words[j]:
                     res.add(word)
         return list(res)
+        '''
