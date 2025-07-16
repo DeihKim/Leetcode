eb0172c5bae93f7e625c8938517ba37e704ef66b
Cing Sian Deih Kim <deihkim442@gmail.com>
2025-07-16 12:36:08 +0630

    1970-SORTING-THE-SENTENCE Stats: Time: 23 ms (7.99%), Space: 12.5 MB (50.41%) - LeetHub

--- 1970-sorting-the-sentence/1970-sorting-the-sentence.py
+++ 1970-sorting-the-sentence/1970-sorting-the-sentence.py
@@ -5,8 +5,17 @@ class Solution(object):
         :rtype: str
         """
         s = s.split()
+        res = [''] * len(s)
+        for i in s:
+            indx = int(i[-1]) - 1
+            res[indx] = i[:-1]
+        return ' '.join(res)
+
+        '''
+        s = s.split()
         dic = {}
         for word in s:
             dic[word[-1]] = word[:-1] # word[-1] returns the last character of the string
         
         return ' '.join([dic[i] for i in sorted(dic)])
+        '''
