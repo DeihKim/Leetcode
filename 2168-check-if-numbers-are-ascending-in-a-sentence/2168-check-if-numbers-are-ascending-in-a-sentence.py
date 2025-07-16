5ad998e6a26ac2f986e517e19c23ddc1542d850c
Cing Sian Deih Kim <deihkim442@gmail.com>
2025-07-16 10:38:13 +0630

    2168-CHECK-IF-NUMBERS-ARE-ASCENDING-IN-A-SENTENCE Stats: Time: 0 ms (100%), Space: 12.4 MB (53.55%) - LeetHub

--- 2168-check-if-numbers-are-ascending-in-a-sentence/2168-check-if-numbers-are-ascending-in-a-sentence.py
+++ 2168-check-if-numbers-are-ascending-in-a-sentence/2168-check-if-numbers-are-ascending-in-a-sentence.py
@@ -4,6 +4,17 @@ class Solution(object):
         :type s: str
         :rtype: bool
         """
+        l1 = s.split()
+        l2 = [int(token) for token in l1 if token.isdigit()]
+        
+        for i in range(len(l2) - 1):
+            if l2[i] >= l2[i + 1]:
+                return False
+        
+        return True
+        
+        '''
+        Original
         last = -1
         tokens = s.split()
         for token in tokens:
@@ -13,3 +24,4 @@ class Solution(object):
                     return False
                 last = num
         return True
+        '''
