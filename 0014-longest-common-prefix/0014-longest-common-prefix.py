26bd0ef1d699de35ae4b795f30bd3953f935b205
Cing Sian Deih Kim <deihkim442@gmail.com>
2025-07-18 12:19:00 +0630

    0014-LONGEST-COMMON-PREFIX Stats: Time: 0 ms (100%), Space: 12.4 MB (59.95%) - LeetHub

--- 0014-longest-common-prefix/0014-longest-common-prefix.py
+++ 0014-longest-common-prefix/0014-longest-common-prefix.py
@@ -4,6 +4,17 @@ class Solution(object):
         :type strs: List[str]
         :rtype: str
         """
+        if not strs:
+            return ""
+        prefix = strs[0]
+        for s in strs[1:]:
+            while not s.startswith(prefix) and prefix:
+                prefix = prefix[:-1]
+            if not prefix:
+                return ""
+        return prefix
+
+        '''
         compareLen = len(strs[0])
         for i in range(compareLen):
             check = strs[0][:compareLen - i]
@@ -11,3 +22,4 @@ class Solution(object):
             if common:
                 return check
         return ""
+        '''
