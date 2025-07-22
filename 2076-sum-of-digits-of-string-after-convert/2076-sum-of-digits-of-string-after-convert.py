cbd2eb5b6c248ccc3d96946c6d500f428adf1c26
Cing Sian Deih Kim <deihkim442@gmail.com>
2025-07-22 07:57:45 +0630

    2076-SUM-OF-DIGITS-OF-STRING-AFTER-CONVERT Stats: Time: 6 ms (73.38%), Space: 12.4 MB (46.4%) - LeetHub

--- 2076-sum-of-digits-of-string-after-convert/2076-sum-of-digits-of-string-after-convert.py
+++ 2076-sum-of-digits-of-string-after-convert/2076-sum-of-digits-of-string-after-convert.py
@@ -5,6 +5,14 @@ class Solution(object):
         :type k: int
         :rtype: int
         """
+        convert = "".join(str(ord(ch) - 96) for ch in s)
+        for i in range(k):
+            total = sum(map(int, convert))
+            convert = str(total)
+        return total
+
+        '''
+        Original
         convert = ""
         for char in s:
             convert += str(ord(char) - ord('a') + 1)
@@ -14,5 +22,4 @@ class Solution(object):
             total = sum(int(num) for num in convert)
             convert = str(total)
         return total
-
-            
+        '''  
