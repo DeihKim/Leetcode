8549bdb9a05124f45284d01698315a6789e28720
Cing Sian Deih Kim <deihkim442@gmail.com>
2025-07-22 08:16:59 +0630

    2298-COUNT-INTEGERS-WITH-EVEN-DIGIT-SUM Stats: Time: 0 ms (100%), Space: 12.3 MB (98.85%) - LeetHub

--- 2298-count-integers-with-even-digit-sum/2298-count-integers-with-even-digit-sum.py
+++ 2298-count-integers-with-even-digit-sum/2298-count-integers-with-even-digit-sum.py
@@ -4,8 +4,12 @@ class Solution(object):
         :type num: int
         :rtype: int
         """
+        return num // 2 if sum(int(digit) for digit in str(num)) % 2 == 0 else (num - 1) // 2
+        '''
+        Original
         count = 0
         for i in range(2, num + 1):
             if sum(list(map(int, str(i)))) % 2 == 0:
                 count += 1
         return count
+        '''
