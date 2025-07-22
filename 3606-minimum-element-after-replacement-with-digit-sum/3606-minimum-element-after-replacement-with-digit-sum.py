b7850c16863a8382ed6f1f93e21b8c191f548336
Cing Sian Deih Kim <deihkim442@gmail.com>
2025-07-22 08:33:23 +0630

    3606-MINIMUM-ELEMENT-AFTER-REPLACEMENT-WITH-DIGIT-SUM Stats: Time: 5 ms (80.21%), Space: 12.4 MB (54.55%) - LeetHub

--- 3606-minimum-element-after-replacement-with-digit-sum/3606-minimum-element-after-replacement-with-digit-sum.py
+++ 3606-minimum-element-after-replacement-with-digit-sum/3606-minimum-element-after-replacement-with-digit-sum.py
@@ -4,7 +4,19 @@ class Solution(object):
         :type nums: List[int]
         :rtype: int
         """
+        min = float('inf')
+        for num in nums:
+            total = 0
+            while num > 0:
+                total += num % 10
+                num //= 10
+            if total < min:
+                min = total
+        return min
+        '''
+        Original
         replaced = []
         for num in nums:
             replaced.append(sum(map(int, str(num))))
         return min(replaced)
+        '''
