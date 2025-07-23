3e95db8e272356654c1e93bb053239ea45390379
Cing Sian Deih Kim <deihkim442@gmail.com>
2025-07-23 11:07:05 +0630

    1519-MINIMUM-SUBSEQUENCE-IN-NON-INCREASING-ORDER Stats: Time: 3 ms (73.58%), Space: 12.6 MB (31.13%) - LeetHub

--- 1519-minimum-subsequence-in-non-increasing-order/1519-minimum-subsequence-in-non-increasing-order.py
+++ 1519-minimum-subsequence-in-non-increasing-order/1519-minimum-subsequence-in-non-increasing-order.py
@@ -4,9 +4,23 @@ class Solution(object):
         :type nums: List[int]
         :rtype: List[int]
         """
+        nums.sort(reverse = True)
+        total = sum(nums)
+        res = []
+        currentTotal = 0
+        for num in nums:
+            currentTotal += num
+            res.append(num)
+            if currentTotal > total - currentTotal:
+                break
+        return res
+        
+        '''
+        Original
         if len(nums) == 1 or (len(nums) == 2 and nums[0] == nums[1]):
             return nums
         nums.sort(reverse = True)
         for i in range(1, len(nums)):
             if sum(nums[:i]) > sum(nums[i:]):
                 return nums[:i]
+        '''
