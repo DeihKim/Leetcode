668a637da45517361967962cb828908497618d30
Cing Sian Deih Kim <deihkim442@gmail.com>
2025-07-24 10:50:12 +0630

    2274-KEEP-MULTIPLYING-FOUND-VALUES-BY-TWO Stats: Time: 0 ms (100%), Space: 12.6 MB (16.2%) - LeetHub

--- 2274-keep-multiplying-found-values-by-two/2274-keep-multiplying-found-values-by-two.py
+++ 2274-keep-multiplying-found-values-by-two/2274-keep-multiplying-found-values-by-two.py
@@ -5,8 +5,16 @@ class Solution(object):
         :type original: int
         :rtype: int
         """
+        nums = set(nums)
+        while original in nums:
+            original *= 2
+        return original
+        
+        '''
+        Original
         nums.sort()
         for num in nums:
             if num == original:
                 original *= 2
         return original
+        '''
