e1cdfff0a5eb17d2bfd0233ca6b525e11f8c0dc7
Cing Sian Deih Kim <deihkim442@gmail.com>
2025-07-25 17:45:26 +0630

    2469-LONGEST-SUBSEQUENCE-WITH-LIMITED-SUM Stats: Time: 11 ms (88.68%), Space: 12.7 MB (75.47%) - LeetHub

--- 2469-longest-subsequence-with-limited-sum/2469-longest-subsequence-with-limited-sum.py
+++ 2469-longest-subsequence-with-limited-sum/2469-longest-subsequence-with-limited-sum.py
@@ -6,6 +6,20 @@ class Solution(object):
         :rtype: List[int]
         """
         nums.sort()
+        total = [0] * len(nums)
+        total[0] = nums[0]
+        for i in range(1, len(nums)):
+            total[i] = total[i - 1] + nums[i]
+        
+        res = []
+        for query in queries:
+            index = bisect.bisect(total, query)
+            res.append(index)
+        return res
+
+        '''
+        original
+        nums.sort()
         total = []
         sum = 0
         for num in nums:
@@ -22,3 +36,4 @@ class Solution(object):
                     break
             ans.append(count)
         return ans
+        '''
