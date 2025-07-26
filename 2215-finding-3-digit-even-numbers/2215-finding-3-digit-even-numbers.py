094a883327acf9b72f10c921f07530dc61e918f6
Cing Sian Deih Kim <deihkim442@gmail.com>
2025-07-26 10:39:15 +0630

    2215-FINDING-3-DIGIT-EVEN-NUMBERS Stats: Time: 4 ms (95.53%), Space: 12.5 MB (47.49%) - LeetHub

--- 2215-finding-3-digit-even-numbers/2215-finding-3-digit-even-numbers.py
+++ 2215-finding-3-digit-even-numbers/2215-finding-3-digit-even-numbers.py
@@ -7,9 +7,26 @@ class Solution(object):
         """
         freq = Counter(digits)
         res = []
+        for i in range(1, 10):
+            if freq[i] == 0: continue
+            freq[i] -= 1
+            for j in range(10):
+                if freq[j] == 0: continue
+                freq[j] -= 1
+                for k in range(0, 10, 2):
+                    if freq[k] == 0: continue
+                    res.append(i * 100 + j * 10 + k)
+                freq[j] += 1
+            freq[i] += 1
+        return res
+
+        '''
+        freq = Counter(digits)
+        res = []
         for num in range(100, 1000, 2):
             integer = [num // 100, (num % 100) // 10, num % 10]
             intFreq = Counter(integer)
-            if all(freq[n] >= intFreq[n] for n in integer):
+            if all(freq[n] >= intFreq[n] for n in intFreq):
                 res.append(num)
         return res
+        '''
