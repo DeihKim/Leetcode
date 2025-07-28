104358fb9c0e5b550fdca05944c8977b1381966f
Cing Sian Deih Kim <deihkim442@gmail.com>
2025-07-28 17:29:06 +0630

    2624-DIFFERENCE-BETWEEN-ELEMENT-SUM-AND-DIGIT-SUM-OF-AN-ARRAY Stats: Time: 11 ms (96.73%), Space: 12.6 MB (84.73%) - LeetHub

--- 2624-difference-between-element-sum-and-digit-sum-of-an-array/2624-difference-between-element-sum-and-digit-sum-of-an-array.py
+++ 2624-difference-between-element-sum-and-digit-sum-of-an-array/2624-difference-between-element-sum-and-digit-sum-of-an-array.py
@@ -4,9 +4,20 @@ class Solution(object):
         :type nums: List[int]
         :rtype: int
         """
+        eSum = sum(nums)
+        dSum = 0
+        for num in nums:
+            while num > 0:
+                dSum += num % 10
+                num //= 10
+        return abs(eSum - dSum)
+        
+        '''
+        Original
         eSum = 0
         dSum = 0
         for num in nums:
             eSum += num
             dSum += sum(map(int, str(num)))
         return abs(eSum - dSum)
+        '''
