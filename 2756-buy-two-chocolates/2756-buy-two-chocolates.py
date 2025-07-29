94bb4da795ea0f801834b1f235ce978e6f5ec47b
Cing Sian Deih Kim <deihkim442@gmail.com>
2025-07-29 20:27:33 +0630

    2756-BUY-TWO-CHOCOLATES Stats: Time: 2 ms (51.55%), Space: 12.4 MB (80.75%) - LeetHub

--- 2756-buy-two-chocolates/2756-buy-two-chocolates.py
+++ 2756-buy-two-chocolates/2756-buy-two-chocolates.py
@@ -5,7 +5,23 @@ class Solution(object):
         :type money: int
         :rtype: int
         """
+        minPrice = float('inf')
+        sminPrice = float('inf')
+        for i in range(len(prices)):
+            if prices[i] < minPrice:
+                sminPrice = minPrice
+                minPrice = prices[i]
+            elif prices[i] < sminPrice:
+                sminPrice = prices[i]
+        cost = minPrice + sminPrice
+        if cost <= money:
+            return money - cost
+        return money
+
+        '''
+        Original
         prices.sort()
         if prices[0] + prices[1] <= money:
             return money - (prices[0] + prices[1])
         return money
+        '''
