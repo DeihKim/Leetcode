6728ac311cc561c9c3a27a84a5a781ddea80c754
Cing Sian Deih Kim <deihkim442@gmail.com>
2025-07-30 10:11:40 +0630

    2752-SUM-MULTIPLES Stats: Time: 35 ms (50.51%), Space: 12.5 MB (11.69%) - LeetHub

--- 2752-sum-multiples/2752-sum-multiples.py
+++ 2752-sum-multiples/2752-sum-multiples.py
@@ -6,10 +6,6 @@ class Solution(object):
         """
         total = 0
         for i in range(3, n + 1):
-            if i % 3 == 0:
-                total += i
-            elif i % 5 == 0:
-                total += i
-            elif i % 7 == 0:
+            if i % 3 == 0 or i % 5 == 0 or i % 7 == 0:
                 total += i
         return total
