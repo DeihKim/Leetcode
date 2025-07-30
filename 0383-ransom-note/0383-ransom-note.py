f6b82a5b2b9fb5b7aa2bc587a080922ca8488494
Cing Sian Deih Kim <deihkim442@gmail.com>
2025-07-30 10:55:22 +0630

    0383-RANSOM-NOTE Stats: Time: 0 ms (100%), Space: 12.7 MB (78.59%) - LeetHub

--- 0383-ransom-note/0383-ransom-note.py
+++ 0383-ransom-note/0383-ransom-note.py
@@ -6,9 +6,20 @@ class Solution(object):
         :type magazine: str
         :rtype: bool
         """
+        alphabet = [0] * 26
+        for c in ransomNote:
+            indx = ord(c) - ord('a')
+            i = magazine.find(c, alphabet[indx])
+            if i == -1:
+                return False
+            alphabet[indx] = i + 1
+        return True
+        '''
+        Original
         ransomNote = Counter(ransomNote)
         magazine = Counter(magazine)
         for char in ransomNote:
             if ransomNote[char] > magazine[char]:
                 return False
         return True
+        '''
