d0bcac3b90577cecfa3540c98ebf8040df7c5079
Cing Sian Deih Kim <deihkim442@gmail.com>
2025-07-30 12:21:05 +0630

    1112-FIND-WORDS-THAT-CAN-BE-FORMED-BY-CHARACTERS Stats: Time: 24 ms (91.67%), Space: 12.7 MB (79.69%) - LeetHub

--- 1112-find-words-that-can-be-formed-by-characters/1112-find-words-that-can-be-formed-by-characters.py
+++ 1112-find-words-that-can-be-formed-by-characters/1112-find-words-that-can-be-formed-by-characters.py
@@ -6,6 +6,27 @@ class Solution(object):
         :type chars: str
         :rtype: int
         """
+        def canForm(self, word, alphaCount):
+            wCount = [0] * 26
+            for ch in word:
+                indx = ord(ch) - ord('a')
+                wCount[indx] += 1
+                if wCount[indx] > alphaCount[indx]:
+                    return False
+            return True
+            
+        alphaCount = [0] * 26
+        for ch in chars:
+            alphaCount[ord(ch) - ord('a')] += 1
+        
+        total = 0
+        for word in words:
+            if canForm(self, word, alphaCount):
+                total += len(word)
+        
+        return total
+        
+        '''Original
         chars = Counter(chars)
         total = 0
         for word in words:
@@ -17,3 +38,4 @@ class Solution(object):
             if good:
                 total += len(word)
         return total
+        '''
