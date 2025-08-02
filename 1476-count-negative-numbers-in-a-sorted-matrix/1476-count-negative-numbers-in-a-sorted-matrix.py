7ea10a2add7240d702e020cce6ff2365e9bc56ff
Cing Sian Deih Kim <deihkim442@gmail.com>
2025-08-02 09:54:06 +0630

    1476-COUNT-NEGATIVE-NUMBERS-IN-A-SORTED-MATRIX Stats: Time: 0 ms (100%), Space: 13.1 MB (90.22%) - LeetHub

--- 1476-count-negative-numbers-in-a-sorted-matrix/1476-count-negative-numbers-in-a-sorted-matrix.py
+++ 1476-count-negative-numbers-in-a-sorted-matrix/1476-count-negative-numbers-in-a-sorted-matrix.py
@@ -4,9 +4,24 @@ class Solution(object):
         :type grid: List[List[int]]
         :rtype: int
         """
+        count = 0
+        m, n = len(grid), len(grid[0])
+        col, row = 0, m - 1
+        
+        while row >= 0 and col < n:
+            if grid[row][col] < 0:
+                count += n - col
+                row -= 1
+            else:
+                col += 1
+        
+        return count
+
+        '''Origianl
         total = 0
         for arr in grid:
             neg = bisect.bisect_left(sorted(arr), 0)
             total += neg
         
         return total
+        '''
