8ea91eee8e3c6f4aa15a13c2478990e59c517351
Cing Sian Deih Kim <deihkim442@gmail.com>
2025-08-05 15:40:13 +0630

    2525-COUNT-NUMBER-OF-DISTINCT-INTEGERS-AFTER-REVERSE-OPERATIONS Stats: Time: 207 ms (98.17%), Space: 37 MB (54.13%) - LeetHub

--- 2525-count-number-of-distinct-integers-after-reverse-operations/2525-count-number-of-distinct-integers-after-reverse-operations.py
+++ 2525-count-number-of-distinct-integers-after-reverse-operations/2525-count-number-of-distinct-integers-after-reverse-operations.py
@@ -4,8 +4,19 @@ class Solution(object):
         :type nums: List[int]
         :rtype: int
         """
+        rev = []
+        for num in nums:
+            if num < 10:
+                rev.append(num)
+            else:
+                rev.append(int(str(num)[::-1]))
+        
+        return len(set(rev + nums))
+
+        '''
         nums = set(nums)
         rev = []
         for num in nums:
             rev.append(int(str(num)[::-1]))
         return len(nums | set(rev)) # | returns the union of nums and set(rev)
+        '''
