3fec9539ba36c13fdb8a1016e9de809faa9c4b34
Cing Sian Deih Kim <deihkim442@gmail.com>
2025-08-06 21:04:26 +0630

    1013-FIBONACCI-NUMBER Stats: Time: 19 ms (56.3%), Space: 12.6 MB (9.52%) - LeetHub

--- 1013-fibonacci-number/1013-fibonacci-number.py
+++ 1013-fibonacci-number/1013-fibonacci-number.py
@@ -4,9 +4,19 @@ class Solution(object):
         :type n: int
         :rtype: int
         """
-        def f(n):
+        if n <= 1:
+            return n
+        
+        a, b = 0, 1
+        for _ in range(2, n + 1):
+            a, b = b, a + b
+        
+        return b
+
+        '''
+        def f(n:)
             if n <= 1:
-                return n
-            
+                return n            
             return f(n - 2) + f(n - 1)
         return f(n)
+        '''
