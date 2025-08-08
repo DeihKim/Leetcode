73fb4af89c656e59c63ce6cccf5e4f605e3a3b52
Cing Sian Deih Kim <deihkim442@gmail.com>
2025-08-09 01:49:52 +0630

    2238-A-NUMBER-AFTER-A-DOUBLE-REVERSAL Stats: Time: 0 ms (100%), Space: 12.4 MB (48.53%) - LeetHub

--- 2238-a-number-after-a-double-reversal/2238-a-number-after-a-double-reversal.py
+++ 2238-a-number-after-a-double-reversal/2238-a-number-after-a-double-reversal.py
@@ -4,6 +4,4 @@ class Solution(object):
         :type num: int
         :rtype: bool
         """
-        if 0 <= num <= 9:
-            return True
-        return True if int(str(int(str(num)[::-1]))[::-1]) == num else False
+        return num == 0 or num % 10 != 0
