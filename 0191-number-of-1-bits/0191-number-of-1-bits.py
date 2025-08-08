357eb7ac854cc4d9fde27d68bbb07c2053ee3a9f
Cing Sian Deih Kim <deihkim442@gmail.com>
2025-08-08 22:09:44 +0630

    0191-NUMBER-OF-1-BITS Stats: Time: 0 ms (100%), Space: 12.5 MB (52.03%) - LeetHub

--- 0191-number-of-1-bits/0191-number-of-1-bits.py
+++ 0191-number-of-1-bits/0191-number-of-1-bits.py
@@ -4,12 +4,28 @@ class Solution(object):
         :type n: int
         :rtype: int
         """
+        binary = []
+        while n >= 1:
+            binary.append(n % 2)
+            if n == 1:
+                break
+            n //= 2
+            
+        binary = binary[::-1]
+        bit = 0
+        for digit in binary:
+            if digit == 1:
+                bit += 1
+
+        return bit
+        '''
         binary = bin(n)[2:]
         bit = 0
         for char in binary:
             if char == '1':
                 bit += 1
         return bit
+        '''
         '''
         bit = 0
         while n > 0:
