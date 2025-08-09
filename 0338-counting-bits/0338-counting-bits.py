5a1b9a4807189b2e635b798511a97bb231712fd9
Cing Sian Deih Kim <deihkim442@gmail.com>
2025-08-10 04:19:07 +0630

    0338-COUNTING-BITS Stats: Time: 21 ms (28.76%), Space: 17.8 MB (75.36%) - LeetHub

--- 0338-counting-bits/0338-counting-bits.py
+++ 0338-counting-bits/0338-counting-bits.py
@@ -4,8 +4,16 @@ class Solution(object):
         :type n: int
         :rtype: List[int]
         """
+        res = []
+        for i in range(n + 1):
+            res.append(str(bin(i)).count('1'))
+        
+        return res
+        
+        '''
         counter = [0]
         for i in range(1, n + 1):
             counter.append(counter[i >> 1] + i % 2)
         return counter
+        '''
             
