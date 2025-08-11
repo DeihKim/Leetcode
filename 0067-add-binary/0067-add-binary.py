98fb9dd18620e231b3f4de6ce5921c8fe617c182
Cing Sian Deih Kim <deihkim442@gmail.com>
2025-08-12 01:07:08 +0630

    0067-ADD-BINARY Stats: Time: 4 ms (49.2%), Space: 12.4 MB (85.34%) - LeetHub

--- 0067-add-binary/0067-add-binary.py
+++ 0067-add-binary/0067-add-binary.py
@@ -5,4 +5,22 @@ class Solution(object):
         :type b: str
         :rtype: str
         """
+        res = []
+        i = len(a) - 1
+        j = len(b) - 1
+        carry = 0
+        while i >= 0 or j >= 0 or carry:
+            if i >= 0:
+                carry += int(a[i])
+                i -= 1
+            if j >= 0:
+                carry += int(b[j])
+                j -= 1
+            res.append(str(carry % 2))
+            carry //= 2
+        
+        return ''.join(reversed(res))
+
+        '''
         return bin(int(a, 2) + int(b, 2))[2:]
+        '''
