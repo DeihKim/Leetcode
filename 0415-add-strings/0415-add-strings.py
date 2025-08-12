55e0a40669b1f03af45b9417cc195f7e698b5736
Cing Sian Deih Kim <deihkim442@gmail.com>
2025-08-12 23:15:40 +0630

    0415-ADD-STRINGS Stats: Time: 6 ms (40.17%), Space: 12.5 MB (73.53%) - LeetHub

--- 0415-add-strings/0415-add-strings.py
+++ 0415-add-strings/0415-add-strings.py
@@ -11,6 +11,29 @@ class Solution(object):
         carry = 0
         while i >= 0 or j >= 0 or carry:
             if i >= 0:
+                digit1 = ord(num1[i]) - ord('0')
+                i -= 1
+            else:
+                digit1 = 0
+
+            if j >= 0:
+                digit2 = ord(num2[j]) - ord('0')
+                j -= 1
+            else:
+                digit2 = 0
+
+            carry += digit1 + digit2
+            res += chr(carry % 10 + ord('0'))
+            carry //= 10
+        return res[::-1]
+
+        '''
+        i = len(num1) - 1
+        j = len(num2) - 1
+        res = ''
+        carry = 0
+        while i >= 0 or j >= 0 or carry:
+            if i >= 0:
                 carry += int(num1[i])
                 i -= 1
             if j >= 0:
@@ -19,3 +42,4 @@ class Solution(object):
             res += str(carry % 10)
             carry //= 10
         return res[::-1]
+        '''
